e3eeb1fa9628a34294a0d2e15b6c8c703ab2934a
Simon Hampton <hotbelgo@gmail.com>
2016-08-29 21:57:53 +0200

    bugfixes

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -3,5 +3,5 @@ use Mix.Config
 # In this file, we keep production configuration that
 # you likely want to automate and keep it away from
 # your version control system.
-config :meepg, Meepg.Endpoint,
+config :epchat, EPChat.Endpoint,
   secret_key_base: "YZZjvcJryY+4ObFWabZXFomL36h2NjlulZgnjczvK5hIDPhjgYbu+MFC9wop2zax"
